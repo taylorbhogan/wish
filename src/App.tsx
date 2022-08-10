124de7b1ba6c23de06606a1146ece90b87271a63
Taylor Hogan <taylorb.hogan@gmail.com>
2022-08-09 20:34:57 -0700

    Connect Signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div className="App">
-      React App
+      <Signup />
     </div>
   );
 }
