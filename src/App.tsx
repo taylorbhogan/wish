b5535153760102ee9943bfe0d680ef6d196301a6
Taylor Hogan <taylorb.hogan@gmail.com>
2022-08-23 11:14:28 -0700

    Set up file structure for Splash, Login, Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home';
 import Signup from './components/Signup';
+import Login from './components/Login';
+import Splash from './components/Splash';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Signup />}></Route>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/signup' element={<Signup />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='/splash' element={<Splash />}></Route>
     </Routes>
   );
 }
