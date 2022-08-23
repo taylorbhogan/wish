b6d8397cc99fe5a35caa9b020267beed1e473200
Taylor Hogan <taylorb.hogan@gmail.com>
2022-08-23 15:27:31 -0700

    Style and implement navigation using WishlistLink

--- src/components/WishlistLink/WishlistLink.tsx
+++ src/components/WishlistLink/WishlistLink.tsx
@@ -1,4 +1,5 @@
-import styles from "./WishlistLink.module.css"
+import { Link } from "react-router-dom";
+import styles from "./WishlistLink.module.css";
 
 type WishlistLinkProps = {
   name?: string;
@@ -8,10 +9,18 @@ type WishlistLinkProps = {
 
 const WishlistLink = ({ name, id, isMine }: WishlistLinkProps): JSX.Element => {
   return (
-    <div className={styles.container}>
+    <Link
+      to={`/${id}`}
+      style={
+        isMine
+          ? { backgroundColor: "var(--blue200)" }
+          : { backgroundColor: "var(--blue900)" }
+      }
+      className={styles.container}
+    >
       <h1>{isMine ? "My List" : `${name}'s List`}</h1>
       <p>Gift Ideas Claimed: </p>
-    </div>
+    </Link>
   );
 };
 
